e91288f8c1592fb11bf642b13cdc04c9baf8706a
mohammadAlawnah <malawnah@gmail.com>
2023-12-10 23:49:12 +0200

    Add blur to the about landing page

--- src/pages/landing/Landing.js
+++ src/pages/landing/Landing.js
@@ -1,7 +1,6 @@
 import React from 'react'
-import AboutLanding from '../../components/aboutLanding/AboutLanding'
 export default function Landing() {
   return (
-    <AboutLanding />
+    <></>
     )
 }
