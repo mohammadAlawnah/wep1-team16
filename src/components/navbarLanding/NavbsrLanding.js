be1e971bb04bc456a258184d7f05fad5f6b26c40
mohammadAlawnah <malawnah@gmail.com>
2023-12-10 11:21:51 +0200

    Add project structure

---
+++ src/components/navbarLanding/NavbsrLanding.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function NavbsrLanding() {
+  return (
+    <div>NavbsrLanding</div>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -4,10 +4,34 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.js'
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Landing from './pages/landing/Landing';
+import Events from './pages/events/Events';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Landing />,
+  },
+  {
+    path: "/landing",
+    element: <Landing />,
+  },
+  {
+    path: "/events",
+    element: <Events />,
+  },
+]);
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
