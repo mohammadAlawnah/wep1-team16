d88097ea5b4dac2051eca6c082ab2a89b2d1a078
mohammad ali jaber <jaberaaa8@gmail.com>
2023-12-12 23:11:34 +0300

    i added my task it contains DownloadApp in many ways

---
+++ src/components/downloadApp/DownloadApp.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './DownloadApp.css'
+import BottonOfDounload from './BottonOfDownload'
+export default function DownloadApp() {
+  return (
+    <div className="DownloadApp">
+        <div className="container mt-5">
+     <h1>People prefer to massage</h1>
+     <p>Frequently sufficient to be unaffected. The furnished she concluded 
+        <br></br>depending procuring concealed.
+</p>
+<BottonOfDounload/>
+<br></br>
+ <p>Easy set-up &bull; Free Forever &bull; Secure</p>           
+        </div>
+    </div>
+  )
+}
--- src/pages/landing/Landing.js
+++ src/pages/landing/Landing.js
@@ -2,13 +2,17 @@ import React from 'react'
 import NavbsrLanding from '../../components/navbarLanding/NavbsrLanding'
 import AboutLanding from '../../components/aboutLanding/AboutLanding'
 import ScreenCompatibility from '../../components/screenCompatibilitySection/ScreenCompatibility'
+import DownloadApp from '../../components/downloadApp/DownloadApp'
 export default function Landing() {
   return (
-        <>
-          <NavbsrLanding />
-          <AboutLanding />
-            <ScreenCompatibility />
-         </>
+    <>
+    <NavbsrLanding />
+    <AboutLanding />
+      <ScreenCompatibility />
+      <DownloadApp/>
+   </>
+
+          
     )
 
 }
