18186630c280cf9f18802e4295f840cb06e6228f
Nora Sarrawi <s12217202@stu.najah.edu>
2023-12-16 21:23:57 +0200

    Finalize the event types section

--- src/pages/landing/Landing.js
+++ src/pages/landing/Landing.js
@@ -1,19 +1,17 @@
 import React from "react";
-import NavbsrLanding from '../../components/navbarLanding/NavbsrLanding'
-import AboutLanding from '../../components/aboutLanding/AboutLanding'
-import ScreenCompatibility from '../../components/screenCompatibilitySection/ScreenCompatibility'
-import MassegesSections from '../../components/massegesSections/MassegesSections'
+import NavbsrLanding from "../../components/navbarLanding/NavbsrLanding";
+import AboutLanding from "../../components/aboutLanding/AboutLanding";
+import ScreenCompatibility from "../../components/screenCompatibilitySection/ScreenCompatibility";
+import MassegesSections from "../../components/massegesSections/MassegesSections";
 import CardsContainer from "../../components/cardsContainer/CardsContainer";
 export default function Landing() {
   return (
-        <>
-            <NavbsrLanding />
-            <AboutLanding />
-            <ScreenCompatibility />
-            <MassegesSections/>
-            <CardsContainer />;
-         </>
-    )
+    <>
+      <NavbsrLanding />
+      <AboutLanding />
+      <ScreenCompatibility />
+      <MassegesSections />
+      <CardsContainer />
+    </>
+  );
 }
-
-
