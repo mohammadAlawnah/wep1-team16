b4865e9600642a98647586db1397c77f7f65fa97
mais-arman <maisarman122@gmail.com>
2023-12-17 23:47:03 +0000

    add responsive to video call section and modify it

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.js'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 import {
   createBrowserRouter,
