96ca06d8e432615ec79b3cfe7f6faf3b95f114eb
mohammad ali jaber <jaberaaa8@gmail.com>
2023-12-20 19:55:39 +0300

    1 added ma task taken about header on the proect

--- src/pages/events/Events.js
+++ src/pages/events/Events.js
@@ -1,8 +1,10 @@
 import React from "react";
 import EventSection from '../../components/eventSection/EventSection'
+import Header from "../../components/header/Header";
 export default function Events() {
   return (
     <>
+    <Header/>
     <EventSection />
     </>
   )
