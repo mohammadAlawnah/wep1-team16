23270812e081ff90d115e7414c417d5c2517778b
mais-arman <maisarman122@gmail.com>
2023-12-21 16:48:35 +0000

    Add Explore Groups Section

--- src/pages/events/Events.js
+++ src/pages/events/Events.js
@@ -1,10 +1,12 @@
 import React from "react";
 import EventSection from '../../components/eventSection/EventSection'
 import Footer from "../../components/footer/Footer";
+import ExploreGroups from "../../components/exploreGroups/ExploreGroups";
 export default function Events() {
   return (
     <>
     <EventSection />
+    <ExploreGroups/>
     <Footer/>
     </>
   )
