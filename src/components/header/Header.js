8911235f3dddcff75a9f39ba381f63e2f103e416
mohammadAlawnah <malawnah@gmail.com>
2023-12-22 19:38:59 +0200

    Add searsh feature and responsive header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,13 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Header.css'
 import EventTypesContainer from "../../components/eventTypesContainer/EventTypesContainer"
+import { Link } from 'react-router-dom'
 export default function Header() {
+  const [nameEvent,setNameEvent] = useState('');
+
+  const search = (event)=>{
+    setNameEvent(event.target.value)
+  }
+
   return (
 
 
- <div className='Header'>
-       <div className='paragra'>
-        <h1 className='paragraOfH'>Find events near you</h1>
+ <div className='Header w-100 mb-5'>
+  <div className='container mb-5'>
+      
+  <div className='paragra'>
+        <h1 className='paragraOfH pt-5'>Find events near you</h1>
         <p className='paragraOfP' >Lets uncover the best pleace to eat,drink and shop nearset to you.</p>
         </div>
        <br></br>
@@ -16,17 +25,20 @@ export default function Header() {
   <div className="card-body">
     <h4 className="card-title">We wiil help you to find all</h4>
     <div className="input-group mb-3">
-    <input type="text" className="form-control" placeholder="What" id="search-name" />
+    <input type="text" className="form-control me-3" placeholder="What" id="search-name" onKeyUp={search} />
     <input type="text" className="form-control" placeholder="Where" id="search-name" />
-    <a href="#" className="btn btn-primary">Search</a>
+    <Link to={`/events/${nameEvent}`}  className="btn btn-primary">Search</Link>
 </div>
    
  </div>
 </div>
-<div className='changeEvant'>
+<div className='changeEvant h-100vh'>
 <EventTypesContainer/>
 </div>
  
+
+  </div>
+
   </div>
   )
 }
