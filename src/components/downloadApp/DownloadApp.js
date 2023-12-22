9faa19d68690fe232dec0281f3171d91cb4e1dc3
mohammad ali jaber <jaberaaa8@gmail.com>
2023-12-22 16:43:26 +0300

    I modify my task talken about dowmloadApp

--- src/components/downloadApp/DownloadApp.js
+++ src/components/downloadApp/DownloadApp.js
@@ -5,13 +5,13 @@ export default function DownloadApp() {
   return (
     <div className="DownloadApp">
         <div className="container mt-5">
-     <h1>People prefer to massage</h1>
-     <p>Frequently sufficient to be unaffected. The furnished she concluded 
+     <h1 className='peopleMasseges'>People prefer to massage</h1>
+     <p className='SentenceAboutApp'>Frequently sufficient to be unaffected. The furnished she concluded 
         <br></br>depending procuring concealed.
 </p>
 <BottonOfDounload/>
 <br></br>
- <p>Easy set-up &bull; Free Forever &bull; Secure</p>           
+ <p className='SentenceAboutApp'>Easy set-up &bull; Free Forever &bull; Secure</p>           
         </div>
     </div>
   )
