767b05b3fa2d2dba4f243ec1861a93ff40c21dda
mohammad ali jaber <jaberaaa8@gmail.com>
2023-12-22 03:51:41 +0300

    I add my task talken about Book of Conference

--- src/pages/events/Events.js
+++ src/pages/events/Events.js
@@ -1,9 +1,11 @@
 import React from "react";
 import EventSection from '../../components/eventSection/EventSection'
+import Conference from "../../components/conference/Conference";
 export default function Events() {
   return (
     <>
     <EventSection />
+    <Conference/>
     </>
   )
 }
